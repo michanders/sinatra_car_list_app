0cd8d5d6517dd890f86c786b7de25c72e6feca28
Michael Anderson <michanders@msn.com>
2018-01-23 22:39:27 +0000

    Updated cars controller

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,4 +1,46 @@
 class CarsController < ApplicationController
 
+  get "/cars" do
+    redirect_if_not_logged_in
+    @cars = Car.all
+    erb :"/cars/index"
+  end
 
+  get "/cars/new" do
+    redirect_if_not_logged_in
+    erb :"/cars/new"
+  end
+
+  get "/cars/:id/edit" do
+    redirect_if_not_logged_in
+    @car = Car.find_by_id(params[:id])
+    erb :"/cars/edit"
+  end
+
+  post "/cars/:id" do
+    redirect_if_not_logged_in
+    if params["make"] == "" || params["name"] == "" || params["year"] == "" || params["user_id"] == ""
+      redirect to "/cars/#{@car.id}/edit"
+    else
+      @car = Car.find_by_id(params[:id])
+      @car.update(make: params["make"], name: params["name"], year: params["year"], user_id: params["user_id"])
+      redirect to "/cars/#{@car.id}"
+    end
+  end
+
+  get "/cars/:id" do
+    redirect_if_not_logged_in
+    @car = Car.find_by_id(params[:id])
+    erb :"/cars/show"
+  end
+
+  post "/cars" do
+    redirect_if_not_logged_in
+    if params["make"] == "" || params["name"] == "" || params["year"] == "" || params["user_id"] == ""
+      redirect to "/cars/new"
+    else
+      Car.create(make: params["make"], name: params["name"], year: params["year"], user_id: params["user_id"])
+      redirect to "/cars"
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
     if params[:username] == "" || params[:password] == ""
      redirect to "/signup"
     else
-      @user = User.create(username: params[:username], password: params[:password])
+      @user = User.create(username: params["username"], password: params["password"])
        session[:user_id] = @user.id
        redirect to "/users/:id"
      end
@@ -38,8 +38,8 @@ class UsersController < ApplicationController
   end
 
   post "/login" do
-    user = User.find_by(:username => params[:username])
-    if user && user.authenticate(params[:password])
+    user = User.find_by(:username => params["username"])
+    if user && user.authenticate(params["password"])
       session[:user_id] = user.id
       redirect to "/users/:id"
     else
